9cb34ce6abafc22487f6c7686c3cb1572af43d35
johnrobertmcc <john.robert.mcc@gmail.com>
2021-01-24 14:46:59 -0500

    added libraries, started react, added framework

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Game from './frontend/game';
+
+function App() {
+  return (
+    <div className="App">
+    Scrabble
+    <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/game.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Game extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                testing
+            </div>
+        )
+    }
+}
