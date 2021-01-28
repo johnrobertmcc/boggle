3c72abb0b969905e541d2701239e539f56ea710f
johnrobertmcc <john.robert.mcc@gmail.com>
2021-01-28 17:07:15 -0500

    and now we makin boggle

--- src/frontend/game.jsx
+++ src/frontend/game.jsx
@@ -1,20 +1,70 @@
 import React from 'react';
-import * as ScrabbleBoard from './logic';
-// import {Tiles} from './tiles';
+import * as BoggleBoard from './logic';
 
 export default class Game extends React.Component{
 
     constructor(props) {
         super(props);
-        const board = new ScrabbleBoard.Board(4);
-        // debugger
-        const bag = new ScrabbleBoard.Tiles().bag;
-        this.state = { board: board, bag: bag, collected: '' };
-        this.baseState = this.state;
+        const board = new BoggleBoard.Board(4);
+        this.state = { 
+            board: board, 
+            collected: '', 
+            time: {}, 
+            seconds: 120 
+        };
 
+        this.baseState = this.state;
+        this.timer = 0;
+        this.startTimer = this.startTimer.bind(this);
         this.printBoard = this.printBoard.bind(this);
-        this.countdown = this.countdown.bind(this);
+        this.countDown = this.countDown.bind(this);
     }
+
+
+    secondsToTime(secs){
+        let hours = Math.floor(secs / (60 * 60));
+
+        let divisor_for_minutes = secs % (60 * 60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+
+        let obj = {
+        "h": hours,
+        "m": minutes,
+        "s": seconds
+        };
+        return obj;
+    }
+
+    componentDidMount() {
+        let timeLeftVar = this.secondsToTime(this.state.seconds);
+        this.setState({ time: timeLeftVar });
+    }
+
+    startTimer() {
+
+        if (this.timer == 0 && this.state.seconds > 0) {
+        this.timer = setInterval(this.countDown, 1000);
+        }
+    }
+
+    countDown() {
+        // Remove one second, set state so a re-render happens.
+        let seconds = this.state.seconds - 1;
+        this.setState({
+        time: this.secondsToTime(seconds),
+        seconds: seconds,
+        });
+        
+        // Check if we're at zero.
+        if (seconds == 0) { 
+        clearInterval(this.timer);
+        this.setState(this.baseState)
+        }
+    }
+
 
      printBoard(){
         let {board} = this.state;
@@ -28,17 +78,6 @@ export default class Game extends React.Component{
 
     }
 
-    countdown(){
-        var timeleft = 10;
-        var downloadTimer = setInterval(function(){
-        if(timeleft <= 0){
-            clearInterval(downloadTimer);
-        }
-        document.getElementById("progressBar").value = 10 - timeleft;
-        timeleft -= 1;
-        }, 1000);
-    }
-
     render(){
 
         return(
@@ -46,7 +85,10 @@ export default class Game extends React.Component{
                 <h1>Boggle</h1>
 
                 <div className='testing'>
-                    <progress value="0" max="10" id="progressBar"></progress>
+                    <div>
+                        <button onClick={this.startTimer}>Start</button>
+                        m: {this.state.time.m} s: {this.state.time.s}
+                    </div>
                     <div className='game-board'>
                         {/* <div>Remaining Tiles: {this.state.bag}</div> */}
                         {this.printBoard()}
--- src/frontend/logic.js
+++ src/frontend/logic.js
@@ -1,4 +1,5 @@
 export class Board {
+
   constructor(gridSize) {
     this.grid = []
     this.gridSize = gridSize
@@ -8,26 +9,10 @@ export class Board {
   generateBoard() {
     for(let i = 0; i < this.gridSize; i++){
       let row = [];
-      let half = (this.gridSize - 1)/2;
 
       for(let j = 0; j < this.gridSize; j++){
-        let value;
-        // value = i == j ? 2 : 1;
-        if(i === j && i !== half){
-          value = 2;
-        }else if(i + j === this.gridSize - 1 && i !== half){
-          value = 2
-        }else if(i === half && j === half ){
-          value = 4
-        }else if(i + j === this.gridsize - 1){
-          value = 3
-        }else{
-          value = 1
-        }
-        
-
-        let space = new Place(value, [i,j]);
-        row.push(space.place)
+        let space = new Place([i,j]);
+        row.push(<p onClick={() =>console.log(space.place)}>{space.place}</p>)
       }
       this.grid.push(row)
     }
@@ -35,53 +20,18 @@ export class Board {
   }
 }
 
-export class Tiles{
-
-  constructor(){
-
-    this.bag = [];
-    this.generateBag()
-
-  }
-
-  generateBag(){
-    let test = new Tile('E', 1);
-    this.bag.push(test.letter)
-  }
-}
-
-class Tile{
-    
-    constructor(letter, value){
-        this.letter = letter;
-        this.value = value;
-    }
-}
 
 class Place{
 
-  constructor(value, position){
-    this.value = value;
+  constructor(position){
     this.place = [];
     this.position = position;
     this.generateImage();
   }
 
-  image(value){
-    switch (value) {
-      case 3:
-        return (<div onClick={() => console.log(`${this.position}, ${this.value}`)}>[3]</div>);
-      case 2:
-        return (<div onClick={() => console.log(`${this.position}, ${this.value}`)}>[2]</div>);
-      case 4:
-        return (<div onClick={() => console.log(`${this.position}, ${this.value}`)}>[X]</div>);
-      default:
-        return (<div onClick={() => console.log(`${this.position},  ${this.value}`)}>[1]</div>);
-    }
-  }
 
   generateImage(){
-    this.place.push(this.image(this.value))
+    this.place.push('X')
   }
 }
 
